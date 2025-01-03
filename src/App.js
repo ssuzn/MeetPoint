e93487a669d61171e393683d8bee713ca5b5c550
ssuzn <tnwls_723@naver.com>
2025-01-03 17:36:41 +0900

    feat(#11): footer 구현

--- src/App.js
+++ src/App.js
@@ -1,22 +1,40 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
+import styled from "styled-components";
 
 import Loading from "./components/Loading";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 import MainPage from "./pages/MainPage";
 import MidFindPage from "./pages/MidFindPage";
 
 function App() {
   return (
-    <React.Fragment>
+    <Container>
       <Loading />
       <Header />
-      <Routes>
-        <Route path="/" element={<MainPage />} />
-        <Route path="/find-midpoint" element={<MidFindPage />} />
-      </Routes>
-    </React.Fragment>
+
+      <Page>
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/find-midpoint" element={<MidFindPage />} />
+        </Routes>
+      </Page>
+
+      <Footer />
+    </Container>
   )  
 }
 
 export default App;
+
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
+`;
+
+const Page = styled.div`
+  flex: 1;
+  background-color: #f9f9f9;
+`;
